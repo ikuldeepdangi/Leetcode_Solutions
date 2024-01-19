0e7f662d1e660015bdcd8ddbbd7e640bd433c544
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2024-01-19 12:12:06 +0530

    Time: 2 ms (77.69%) | Memory: 46.1 MB (25.52%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,37 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n= nums.length;
-        
-        ArrayList<Integer> temp = new ArrayList<>();
-    
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                temp.add(nums[i]);
+        int nonZeroIndex = 0;
+
+        // Move non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[nonZeroIndex];
+                nums[nonZeroIndex] = temp;
+                nonZeroIndex++;
             }
         }
-        
-        int nt = temp.size();
-        for(int i=0;i<nt;i++){
-            nums[i]=temp.get(i);
-        }
-        for(int i = nt;i<n;i++){
-            nums[i]=0;
-        }
-
     }
 }
+ 
+
+
+
+
+// int n= nums.length;
+        
+        // ArrayList<Integer> temp = new ArrayList<>();
+    
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=0){
+        //         temp.add(nums[i]);
+        //     }
+        // }
+        
+        // int nt = temp.size();
+        // for(int i=0;i<nt;i++){
+        //     nums[i]=temp.get(i);
+        // }
+        // for(int i = nt;i<n;i++){
+        //     nums[i]=0;
+        // }
