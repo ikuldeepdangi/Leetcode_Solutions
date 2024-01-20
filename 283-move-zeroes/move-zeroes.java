6a911402368f9412132feb83031db2b1ab45fcd5
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2024-01-20 10:57:41 +0530

    Time: 2 ms (77.96%) | Memory: 45.8 MB (46.29%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nonZeroIndex = 0;
-
+        int j = 0;
+        
         // Move non-zero elements to the front
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[nonZeroIndex];
-                nums[nonZeroIndex] = temp;
-                nonZeroIndex++;
-            }
-        }
+       
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 0 ){
+               int temp = nums[i];
+               nums[i] = nums[j];
+               nums[j] = temp;
+            j++;
+      }
+  }
     }
 }
+
+  
  
 
 
