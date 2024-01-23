ec5b32290c0315695bc3aac85c58ddb140548cd7
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2024-01-23 09:56:17 +0530

    Time: 33 ms (51.64%) | Memory: 45.2 MB (8.51%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -8,6 +8,7 @@ class Solution {
                 if(nums[i]+nums[j] == target){
                     arr[0]=i;
                     arr[1]=j;
+                    break;
                 }
             }
         }
