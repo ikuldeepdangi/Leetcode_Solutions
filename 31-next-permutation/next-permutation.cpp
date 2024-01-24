37eede0f241a34f94995df70a7b4589f6f2dc3c7
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2024-01-24 10:50:17 +0530

    Time: 8 ms (17.25%) | Memory: 14.6 MB (5.29%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    void nextPermutation(std::vector<int>& nums) {
+       
+            std::next_permutation(nums.begin(), nums.begin() + nums.size());
+
+    }
+};
