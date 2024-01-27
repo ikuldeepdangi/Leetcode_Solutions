cb4491f400cf6aedf6047bffec91fff6ea2e401c
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2024-01-27 11:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (54.09%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+
+        for(int i=0;i<r;i++){
+           for(int j = i + 1;j<c;j++){
+               // swap for transpose 
+               int temp = matrix[i][j];
+               matrix[i][j] = matrix[j][i];
+               matrix[j][i] = temp;
+           }
+        }
+
+        // reverse matrix;
+        for(int i=0;i<r;i++){
+            int left = 0;
+            int right =c-1;
+
+            while(left<right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];;
+                matrix[i][right]= temp;
+                left++;
+                right--;
+            }
+
+               
+           }
+        }
+    }
