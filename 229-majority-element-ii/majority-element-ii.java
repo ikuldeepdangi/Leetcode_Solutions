fc60cc347eb6d9334823f65743b75f300880184d
Kuldeep Dangi <90251544+ikuldeepdangi@users.noreply.github.com>
2024-04-29 09:14:17 +0530

    Time: 13 ms (25.20%) | Memory: 47.5 MB (74.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -4,42 +4,25 @@ class Solution {
         float t = n/3;
         ArrayList<Integer> ans =  new ArrayList<>();
 
+        HashMap<Integer,Integer> map = new HashMap<>();
+
         for(int i=0;i<n;i++){
-            int count = 0;
-            for(int j=0;j<n;j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
             }
-            if(t < count && !ans.contains(nums[i])){
-               
-                ans.add(nums[i]);
+            else{
+                map.put(nums[i],1);
             }
         }
+        
+
+        for(int i : map.keySet()){
+            if(map.get(i) > t){
+                ans.add(i);
+            }
+        }
+
         return ans;
+        
     }
 }
-
-
-// class Solution {
-//     public List<Integer> majorityElement(int[] nums) {
-//        HashMap<Integer , Integer> h = new HashMap<>();
-       
-//         for (int i=0;i<nums.length;i++)
-//         {
-//         if (h.containsKey(nums[i]))
-//         h.put(nums[i] , h.get(nums[i])+1);
-//         else {
-//             h.put (nums[i] , 1);
-
-//         }
-//         }
-//         List<Integer> arr = new ArrayList<>();
-//         for (int i:h.keySet())
-//         {
-//             if (h.get(i)>nums.length/3)
-//             arr.add(i);
-//         }
-//         return arr; 
-//     }
-// }
